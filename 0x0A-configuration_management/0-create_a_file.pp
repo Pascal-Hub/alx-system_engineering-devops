82e3c0e0e980ed5a617db50ff6cd7c868e2deb77
Pascal-Hub <sequencepascal@gmail.com>
2023-04-25 19:19:45 -0700

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-Using Puppet, create a file in /tmp
-
+# create a file
 file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+	mode    => '0744',
+	owner   => 'www-data',
+	group   => 'www-data',
+	content => 'I love Puppet',
 }
+
