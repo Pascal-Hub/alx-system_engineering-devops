f22556d4b1ab320c3ffe63522f337ab8dc855d0b
Pascal-Hub <sequencepascal@gmail.com>
2023-05-14 10:18:25 -0700

    Load-balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,28 +1,31 @@
-# Installs Nginx server and add custom header
-
-exec {'update':
-	provider => shell,
-	command  => 'sudo apt-get -y update',
-	before   => Exec['install nginx'],
-
+#!/usr/bin/env bash
+# Puppet configuration of custom HTTP Header
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
 }
-
-exec {'install nginx':
-	provider => shell,
-	command  => 'sudo apt-get -y install nginx',
-	before   => Exec['add_header'],
-
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
-
-exec {'add_header':
-	provider     => shell,
-	environment  => ["HOST=${HOSTNAME}"],
-	command      => 'echo "add_header X-Served-By $HOSTNAME;" | sudo tee -a /etc/nginx/nginx.conf > /dev/null',
-
+file_line { 'a':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=VvgHfqXHS-8&list=RDVvgHfqXHS-8&index=1 permanent;',
+  require => Package['nginx'],
 }
-
-exec {'restart nginx':
-	provider  => shell,
-	command   => 'sudo service nginx restart',
-
+file_line { 'b':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+file { '/var/www/html/index.html':
+  content => 'Hello GOD!',
+  require => Package['nginx'],
+}
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
