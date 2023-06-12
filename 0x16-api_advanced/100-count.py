a21a1e869d502d89f47e69cc5b381294be332523
Pascal-Hub <sequencepascal@gmail.com>
2023-06-12 05:26:39 -0700

    API advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,54 +1,47 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
+""" Get all posts recursively and count words """
+
 import requests
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
-    Args list:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
+def count_words(subreddit, word_list, after=None, word_counts=None):
+    """ Get all posts recursively and count words
+
+    Args:
+        subreddit (str): subreddit to search
+        word_list (list): words to search for
+        after (str, optional): pagination. Defaults to None.
+        word_counts (int, optional): word counts. Defaults to None.
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "Google Chrome Version 114.0.5735.110"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
-
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Maleek User Agent 1.0'}
+    params = {'limit': 100}
+    # if there is an after, next page, add it to the params dict
+    if after:
+        params['after'] = after
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+    if response.status_code != 200:
+        return None
+    # get all posts from the response
+    posts = response.json().get('data').get('children')
+    # initialize word_counts dict if it doesn't exist
+    word_counts = word_counts or {}
+    for post in posts:
+        title = post["data"]["title"]
         for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
-
+            if word.lower() in title.lower():
+                # get the value of the key, if it doesn't exist, return 0
+                word_counts[word.lower()] = word_counts.get(
+                    word.lower(), 0) + 1
+    after = response.json()["data"]["after"]
+    # if end of pagination and word_counts is empty, print nothing and return
     if after is None:
-        if len(instances) == 0:
-            print("")
+        if not word_counts:
             return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
-    else:
-        count_words(subreddit, word_list, instances, after, count)
+        # sort the word_counts dict by value in desc, then key in asc order
+        for key, value in sorted(word_counts.items(), key=lambda x: (-x[1],
+                                                                     x[0])):
+            print("{}: {}".format(key.lower(), value))
+        return
+    return count_words(subreddit, word_list, after, word_counts)
